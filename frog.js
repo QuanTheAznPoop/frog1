ffcd1cefac7311c82f0a30287a6118aba61ebe8a
QuanTheAznPoop <nguyenanhquan16122006@gmail.com>
2025-01-14 15:56:30 +0700

    additional code for signup test

--- frog.js
+++ frog.js
@@ -1,26 +1,29 @@
-const { Schema, model, models } = require("mongoose")
-const signup = new Schema({
+const mongoose = require('mongoose')
+
+const signup = new mongoose.Schema({
 
     _username: {
-    
+
     type: String,
     required: true
-    
+
     },
-    
+
     _password: {
-    
+
     type: String,
     required: true
-    
+
     },
 
     _gender: {
-    
+
         type: String,
         required: true
-        
+
         },
     });
 
-module.exports = mongoose.model("Signup", signup)
+const model = mongoose.model("SignupSchema", signup);
+
+module.exports = model;
--- server.js
+++ server.js
@@ -1,10 +1,41 @@
-const username = require("./signup.htm");
-const password = require("./signup.htm");
-const gender = require("./signup.htm");
+const express = require("express");
+const mongoose = require("mongoose");
+const Schema = require("./frog.js");
+const cors = require("cors");
+const app = express();
 
-const accountcreated = require("./frog.js");
-const userData = new accountcreated({
-_username: username, _password: password, _gender: gender
-})
-accountcreated.save().then(dataLog => console.log(dataLog));
-module.exports = {userData};
+app.use(cors()); 
+app.use(express.json()); 
+
+mongoose.connect("mongodb+srv://QuanTheAznPoop:80ObGnh0qEQduunW@ballshd.ixx9f.mongodb.net/?retryWrites=true&w=majority&appName=ballshd", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+app.post("/signup", async (req, res) => {
+    const { username, password, gender } = req.body;
+
+    try {
+        const existed = await Schema.findOne({ _username: username });
+        if (existed) {
+            return res.status(400).json({ message: "Username already exists" });
+        }
+
+        const newUser = new Schema({
+            _username: username,
+            _password: password,
+            _gender: gender,
+        });
+
+        await newUser.save();
+        res.status(201).json({ message: "Your account has been created!" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "An error occurred during signup" });
+    }
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
